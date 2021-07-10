0591c02ace3bd7b193d9333d67de42187ea82cf4
anahitha <63240401+anahitha@users.noreply.github.com>
2021-07-10 12:51:12 +0200

    Add files via upload

---
+++ components/appStackNavigator.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {createStackNavigator} from 'react-navigation-stack';
+import {AppTabNavigator} from './bottomTabNavigator';
+import EventDetails from '../screens/eventDetails';
+import MyUpcomingEvents from '../screens/myEvents';
+import NotificationScreen from '../screens/notifications';
+import SignUp from '../screens/signup';
+import Welcome from '../screens/welcome';
+import Settings from '../screens/update';
+import MyHeader from './header';
+import CreateEvent from '../screens/createEvent'
+
+
+export const AppStackNavigator = createStackNavigator({
+    Home: {screen: AppTabNavigator, 
+    navigationOptions: {headerShown: false}},
+    EventDetails: {screen: EventDetails,
+    navigationOptions: {headerShown: false}},
+    SignUp: {screen: SignUp,
+    navigationOptions: {headerShown: false}},
+    Welcome: {screen: Welcome,
+    navigationOptions: {headerShown: false}},
+},{
+
+    initialRouteName: 'Home'
+})
